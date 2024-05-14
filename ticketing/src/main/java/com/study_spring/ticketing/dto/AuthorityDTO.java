c10b07d74ec15bd425d6f20796b613bce8e351ca
yunji jo <dbtmxlsk2007@naver.com>
2024-05-15 00:27:48 +0900

    add AuthorityDTO

---
+++ ticketing/src/main/java/com/study_spring/ticketing/dto/AuthorityDTO.java
@@ -0,0 +1,12 @@
+package com.study_spring.ticketing.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDTO {
+    private String authorityName;
+}
