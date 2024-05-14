a47f8cc71dc9e9721a9a3e623b3c8c6c675b214a
yunji jo <dbtmxlsk2007@naver.com>
2024-05-15 00:41:17 +0900

    delete login method

--- ticketing/src/main/java/com/study_spring/ticketing/controller/UserController.java
+++ ticketing/src/main/java/com/study_spring/ticketing/controller/UserController.java
@@ -6,16 +6,10 @@ import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.study_spring.ticketing.domain.User;
-import com.study_spring.ticketing.dto.UserCreateDTO;
-import com.study_spring.ticketing.service.UserService;
 
 @RestController
 @RequestMapping("/user")
@@ -32,20 +26,4 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response.getUsername());
     }
 
-    @GetMapping("/user/login")
-    public String loginForm() {
-        return "login";
-    }
-    @PostMapping("/user/login")
-    public String login(@ModelAttribute UserCreateDTO userCreateDTO){
-        UserCreateDTO loginResult = userService.login(userCreateDTO);
-        if (loginResult != null){
-            // 성공
-            return "main";
-        } else {
-          // 실패
-            return "login";
-        }
-
-    }
 }
