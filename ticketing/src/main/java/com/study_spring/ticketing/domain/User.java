0d12b793dee6dcd5b421f10bb86b8c2e2bc5a393
yunji jo <dbtmxlsk2007@naver.com>
2024-05-15 00:39:53 +0900

    update JoinColumns name

--- ticketing/src/main/java/com/study_spring/ticketing/domain/User.java
+++ ticketing/src/main/java/com/study_spring/ticketing/domain/User.java
@@ -42,7 +42,7 @@ public class User {
     @ManyToMany
     @JoinTable(
             name = "user_authority",
-            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            joinColumns = {@JoinColumn(name = "id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
     private Set<Authority> authorities;
 
